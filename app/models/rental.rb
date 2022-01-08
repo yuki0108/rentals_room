952675a22993398958f6b9bd8e08fcb26e6ed1d8
先規　勇樹 <yukisaki80@gmail.com>
2022-01-08 18:12:37 +0900

    投稿のurlを追加

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,2 +1,3 @@
 class Rental < ApplicationRecord
+  mount_uploader :image, ImageUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root :to => 'rentals#index'
+  resources :rentals, only: [:create]
 end
